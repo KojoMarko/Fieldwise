ec07ce418808aa8346ece76704e5f6544eeb2b9d
Caleb Makafui Senyah <calebsenyah@gmail.com>
2025-09-19 12:42:33 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  "projectId": "studio-7671175170-dc56a",
+  "appId": "1:366529567590:web:17f7b26be8f46d86c386a7",
+  "apiKey": "AIzaSyDXMwRlSYKM03z2XatKzOmT9ZnkFDOtBvo",
+  "authDomain": "studio-7671175170-dc56a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "366529567590"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };

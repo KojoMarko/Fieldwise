af3e6ffcbcf6d0988eb74f74e86f0043708666dd
Caleb Makafui Senyah <calebsenyah@gmail.com>
2025-09-19 16:36:53 +0000

    use my logo in place of the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/Field Wise Logo.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"

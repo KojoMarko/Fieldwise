724f9b93620e99f5570a1125499b8ff0db462c7d
Caleb Makafui Senyah <calebsenyah@gmail.com>
2025-11-06 22:41:49 +0000

    where the 0 is i know its for the cost of the product but not every user

--- src/app/dashboard/sales-ledger/components/add-transaction-dialog.tsx
+++ src/app/dashboard/sales-ledger/components/add-transaction-dialog.tsx
@@ -294,7 +294,7 @@ export function AddTransactionDialog({ open, onOpenChange, onAddTransaction }: A
                                     name={`products.${index}.unitPrice`}
                                     render={({ field }) => (
                                     <FormItem className="flex-1">
-                                        <FormLabel className="sr-only">Price</FormLabel>
+                                        <FormLabel className="sr-only">Unit Price</FormLabel>
                                         <FormControl><Input type="number" step="0.01" placeholder="Unit Price" {...field} /></FormControl>
                                         <FormMessage />
                                     </FormItem>

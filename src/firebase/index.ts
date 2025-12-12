b277dd4d2c58c84de84e1098d22f7733f7edff4c
Caleb Makafui Senyah <calebsenyah@gmail.com>
2025-12-12 12:27:14 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/net

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,5 @@
 
+'use client';
 export * from './provider';
 export * from './client-provider';
 export { useCollection } from './firestore/use-collection';
@@ -7,7 +8,7 @@ export { useDoc } from './firestore/use-doc';
 import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getAuth, type Auth } from 'firebase/auth';
-import { getStorage, type FirebaseStorage, uploadBytesResumable } from 'firebase/storage';
+import { getStorage, type FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   "projectId": "studio-7671175170-dc56a",
